03332de256223a572659360674c7b330aeebb307
Larskrs <lkssyv@gmail.com>
2024-04-28 20:05:29 +0200

    Adjusted Auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,27 +1,21 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
-
-import Google from 'next-auth/providers/google'
-// import Discord from "next-auth/providers/discord"
+import Google from "next-auth/providers/google" 
 
 const prisma = new PrismaClient()
  
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [
-
-    Google({
-      clientId:     process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-        },
+  providers: [Google({
+    clientId: process.env.AUTH_GOOGLE_ID,
+    clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    authorization: {
+      params: {
+        prompt: "consent",
+        access_type: "offline",
+        response_type: "code",
       },
-    }),
-
-  ]
+    },
+  }),],
 })
