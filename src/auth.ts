cda53bb5986c170d2b6665f5019af01cf341911e
Larskrs <lkssyv@gmail.com>
2024-04-28 19:57:42 +0200

    Improved google authentication parameters

--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,27 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
-import Google from "next-auth/providers/google" 
+
+import Google from 'next-auth/providers/google'
+// import Discord from "next-auth/providers/discord"
 
 const prisma = new PrismaClient()
  
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Google],
+  providers: [
+
+    Google({
+      clientId:     process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+
+  ]
 })
